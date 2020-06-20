88be0a374f7cc691d872d653cc382c984abb44a4
Laurentiu Diaconu <dlorentsen1@gmail.com>
2020-06-20 10:24:51 +0300

    updated script to do pagination and get investors, not speakers

--- scrape.py
+++ scrape.py
@@ -2,26 +2,36 @@ import json
 import requests
 import pandas as pd
 
-URL = "https://api.cilabs.com/conferences/cc20/lists/speakers?per_page=100"
+base_URL = "https://api.cilabs.com/conferences/cc20/lists/7b29fd4b-fbcf-4a21-bc64-1ccfe9d362af?per_page=50"
+investors = []
 
-response = requests.get(URL)
+for page in range(1, 50):
+    if page != 1:
+        URL = base_URL + str.format("&page={}", page)
+    else:
+        URL = base_URL
 
-data = response.json()
+    print(str.format("Requesting data from {}", URL))
 
-with open('data/raw_response.txt', 'w') as outfile:
-    json.dump(data, outfile)
+    response = requests.get(URL)
 
-speakers = []
+    try:
+        data = response.json()
 
-for speaker in data['data']:
-    speakers.append([
-    speaker['id'],
-    speaker['first_name'],
-    speaker['last_name'],
-    speaker['job_title'],
-    speaker['bio'],
-    speaker['company_name']])
+        # with open('data/raw_response.txt', 'w') as outfile:
+        #     json.dump(data, outfile)
 
-speakers_df = pd.DataFrame.from_records(speakers, columns=['speaker_id', 'first_name', 'last_name', 'job_title', 'bio', 'company_name'])
+        for investor in data['data']:
+            investors.append([
+            investor['id'],
+            investor['first_name'],
+            investor['last_name'],
+            investor['job_title'],
+            investor['bio'],
+            investor['company_name']])
+    except:
+        break
 
-speakers_df.to_csv('data/speakers.csv')
+investors_df = pd.DataFrame.from_records(investors, columns=['id', 'first_name', 'last_name', 'job_title', 'bio', 'company_name'])
+
+investors_df.to_csv('data/investors.csv')
